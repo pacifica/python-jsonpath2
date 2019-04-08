2c49bd8f61a70ec390db26ee4f593e38a3a8f98d
David Brown <dmlb2000@gmail.com>
2019-04-08 10:04:22 -0700

    Fix #15 change homepage url to Github
    According to documentation in the packaging tutorial the url should
    be a reference to the code hosting service.
    
    Signed-off-by: David Brown <dmlb2000@gmail.com>

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     use_scm_version=True,
     setup_requires=['setuptools_scm'],
     license='LGPLv3',
-    url='https://pypi.python.org/pypi/jsonpath2/',
+    url='https://github.com/pacifica/python-jsonpath2/',
     description='JSONPath implementation for Python',
     long_description=open(path.join(
         path.abspath(path.dirname(__file__)),
