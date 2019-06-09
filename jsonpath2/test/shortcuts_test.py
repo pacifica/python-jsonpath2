d15b377f3b75177ce56c8c527a66b988c5f3aea6
Daniel Graña <dangra@gmail.com>
2019-06-09 17:20:37 -0300

    Add test case for jsonpath2.match shortcut

---
+++ jsonpath2/test/shortcuts_test.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""Test package level shortcuts."""
+
+from unittest import TestCase
+import jsonpath2
+
+
+class TestShortcuts(TestCase):
+    """Test package level shortcuts."""
+
+    def test_match(self):
+        """Test match shortcut works."""
+        data = {'hello': 'Hello, World!'}
+        self.assertEqual(
+            [x.current_value for x in jsonpath2.match('$.hello', data)],
+            ['Hello, World!']
+        )
