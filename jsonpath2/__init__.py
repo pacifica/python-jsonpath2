47bd5f621db6ac64b1a62bd4b40798734530867c
Daniel Graña <dangra@gmail.com>
2019-06-09 20:42:32 -0300

    Add handy shortcut to match string path (#25)
    * Add handy shortcut to match string path
    
    This change aims to avoid the boilerplate of applying a JsonPath to a document
    
    ```python
    >>> import jsonpath2 as jsonpath
    >>> doc = {'hello': 'Hello, world!'}
    >>> [m.current_value for m in jsonpath.match('$["hello"]', doc)]
    ```
    
    It resembles the use of `re` module in `re.match(pattern, string)` vs `re.compile(pattern).match(string)`
    
    * Add test case for jsonpath2.match shortcut
    
    * Use shorter idiomatic python for README examples (#24)
    
    * Update README

--- jsonpath2/__init__.py
+++ jsonpath2/__init__.py
@@ -1,3 +1,13 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 """The jsonpath2 module."""
+
+from typing import Generator
+from .path import Path
+from .node import MatchData
+
+
+def match(path_str: str, root_value: object) -> Generator[MatchData, None, None]:
+    """Match root value of the path."""
+    path = Path.parse_str(path_str)
+    return path.match(root_value)
